d86edf7ccbadeef41518ce219ae5ed12270f6fcc
moyang0311 <2733660740@qq.com>
2023-07-06 17:08:51 +0800

    firstCommit test

---
+++ MP-001/pages/index/index.vue
@@ -0,0 +1,41 @@
+<template>
+	<view>
+		<view class="search">
+			<input type="text" placeholder="手机111">
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	page{
+		background-color: #75F9FD;
+	}
+	.search{
+		margin-top: 20rpx;
+		margin: 0 auto;
+		width: 80%;
+		border: 1rpx solid #bbb;
+		height: 60rpx;
+		border-radius: 20px;
+		background-color: #fff;
+	}
+	.search input{
+		margin-left: 20rpx;
+		line-height: 60rpx;
+	}
+</style>
