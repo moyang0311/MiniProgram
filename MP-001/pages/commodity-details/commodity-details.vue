4b61a744768f844ad9df1f5ff8e1e33d2b13a4ac
rqa <771092457@qq.com>
2023-07-10 14:55:53 +0800

    图片

---
+++ MP-001/pages/commodity-details/commodity-details.vue
@@ -0,0 +1,29 @@
+<template>
+	<view class="deteli">
+		<image src="../../static/icon/详情图.png"></image>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.deteli{
+		height: 2000rpx;
+		background-color: aquamarine;
+	}
+	image{
+		width: 100%;
+		height: 744rpx;
+	}
+</style>
